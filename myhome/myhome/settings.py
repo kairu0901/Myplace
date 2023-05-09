14a2c28986dfeace61b0f0c4e02f41d394fdf9f6
kairu <takamiya.work@gmail.com>
2023-05-09 23:48:44 +0900

    admilte3インストール
    base.html作成開始

--- myhome/myhome/settings.py
+++ myhome/myhome/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'simple_history',
     'home.apps.HomeConfig',
+    'bootstrap4',
+    'adminlte3',
 ]
 
 MIDDLEWARE = [
@@ -57,8 +59,8 @@ ROOT_URLCONF = 'myhome.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
+        'DIRS': ['myhome/templates'],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -130,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
 # Default primary key field type
