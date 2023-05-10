034ffce9a6562e0a9a2740e3d054a31714d46207
kairu <takamiya.work@gmail.com>
2023-05-10 18:50:44 +0900

    base.html　サイドメニュー・メインコンテンツ部分設定
    Font Awesomeインストール

--- myplace/home/views.py
+++ myplace/home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views import generic
 
 class HomeView(generic.TemplateView):
-    template_name = "base.html"
+    template_name = "home/home.html"
--- myplace/myplace/settings.py
+++ myplace/myplace/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'bootstrap4',
     'adminlte3',
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
