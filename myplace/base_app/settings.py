a3a5355ef68b557acc68ac95b754b1e6641efeaa
kairu0901 <takamiya.work@gmail.com>
2025-01-10 20:43:17 +0900

    STATIC設定修正

--- myplace/base_app/settings.py
+++ myplace/base_app/settings.py
@@ -137,8 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# 静的ファイルのURL
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+
+# 静的ファイルを収集するディレクトリ
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
