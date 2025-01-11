ff3ebcc83c5b271b0f5a6e0cda9425fcdde89500
kairu0901 <takamiya.work@gmail.com>
2025-01-11 23:03:32 +0900

    デバッグ設定変更

--- myplace/base_app/settings.py
+++ myplace/base_app/settings.py
@@ -25,7 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+env = os.getenv('DJANGO_ENV', 'develop')
+if env == 'production':
+  DEBUG = False
+else:
+  DEBUG = True
 
 ALLOWED_HOSTS = ['52.198.128.181', 'localhost', '127.0.0.1']
 
@@ -137,12 +141,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# 静的ファイルのURL
 STATIC_URL = '/static/'
-
-
-# 静的ファイルを収集するディレクトリ
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATICFILES_DIRS = [
+    BASE_DIR/"static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
