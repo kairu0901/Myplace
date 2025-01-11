a06d0cf069bff5a20f474bce4445d3ae7f9bf4ab
kairu0901 <takamiya.work@gmail.com>
2025-01-11 23:06:36 +0900

    不要な設定削除

--- myplace/base_app/settings.py
+++ myplace/base_app/settings.py
@@ -142,7 +142,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
-BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [
     BASE_DIR/"static",
 ]
